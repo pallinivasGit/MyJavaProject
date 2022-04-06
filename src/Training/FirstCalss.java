2bbb5441c79c6d41c2d30f7d8f4927f9a1b58c10
skila <skila@192.168.1.137>
2022-04-05 23:04:46 -0400

    Upload all project files.

---
+++ src/Training/FirstCalss.java
@@ -0,0 +1,18 @@
+package Training;
+
+public class FirstCalss {
+
+	public static void main(String[] args) {
+		
+		int x=40;
+		int y=50;
+		
+		
+		System.out.println("Welcome to the world of java");
+		System.out.println(x+y);
+		
+		
+        
+	}
+
+}
