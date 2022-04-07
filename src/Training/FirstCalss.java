a29c7e499e3976f326df3407add84b4a5e7db40b
skila <skila@192.168.1.137>
2022-04-06 22:23:35 -0400

    Jenkins integration checking

--- src/Training/FirstCalss.java
+++ src/Training/FirstCalss.java
@@ -12,7 +12,7 @@ public class FirstCalss {
 		System.out.println("Pallavi shopping website for Makeup and bridal collections");
 		System.out.println("Welcome Gayathri and Chandrakala");
 		System.out.println("Added this check to commit 4th time");
-		
+		System.out.println("Checking jenkins workspace update");
 		System.out.println(x+y);
 		
 		
